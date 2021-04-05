9ad5495ddcea6046bd794eb0fcef7154e7b27276
godsavemenow <lvjs@cin.ufpe.br>
2021-04-04 23:42:26 -0300

    heroku server copy

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const bodyParser = require('body-parser');
-const { exit } = require('process');
-
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Accept, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
-
-app.use(bodyParser.json());
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+    console.log(`server listening on port: ${PORT}`)
+  });
 
 function makeid(length) {
     var result           = '';
@@ -30,7 +22,8 @@ app.post('/createroom', function (req = express.Request, res = express.Response)
     room = {
         code : room_code,
         players: [player_name],
-        state: "queue"
+        state: "queue",
+        statearr : [] 
     }
 fs.writeFile('rooms/'+room_code+'.json', JSON.stringify(room), (err) => {
     // throws an error, you could also catch it here
@@ -52,6 +45,10 @@ app.post('/joinroom', function (req = express.Request, res = express.Response) {
             res.send({"failed": "Name already used"});
             return;
         }
+        if(fileObject.state != "queue"){
+            res.send({"failed": "Already in game"});
+            return;
+        }
         fileObject.players.push(player_name)
         console.log(fileObject.players)
         fs.writeFile('rooms/'+room_code+'.json', JSON.stringify(fileObject), (err) => {
@@ -67,14 +64,85 @@ app.post('/joinroom', function (req = express.Request, res = express.Response) {
         res.send({"failed": "the room is full"})
     }
 });
-app.get('/', function (req = express.Request, res = express.Response) {
+app.get('/roomstate', function (req = express.Request, res = express.Response) {
     console.log(req)
     let room_code = req.query.room_code
     const filePath = 'rooms/'+room_code+'.json';
     var fileObject = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     res.send(fileObject);
 })
-
-app.listen(3000, () => {
-  console.log("server run!!!");
+app.get('/roomstart', function (req = express.Request, res = express.Response) {
+    let room_code = req.query.room_code
+    const filePath = 'rooms/'+room_code+'.json';
+    var fileObject = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    if(fileObject.players.length<=4){
+        if(fileObject.state != "queue"){
+            res.send({"failed": "Already in game"});
+            return;
+        }
+        fileObject.state = "puzzle1"
+        fs.writeFile('rooms/'+room_code+'.json', JSON.stringify(fileObject), (err) => {
+            // throws an error, you could also catch it here
+            if (err){ 
+                res.send({"failed": "player not added"});
+                throw err;
+            }
+            // success case, the file was save
+            res.send({"success": "puzzle1"});
+        });
+    }else{
+        res.send({"failed": "the room is full"})
+    }
 });
+app.get('/imright', function (req = express.Request, res = express.Response) {
+    let room_code = req.query.room_code
+    let player_name = req.query.player_name
+    const filePath = 'rooms/'+room_code+'.json';
+    var fileObject = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    if(fileObject.players.length<=4){
+
+        fileObject.statearr.push(player_name)
+        if(fileObject.players.length == fileObject.statearr.length){
+            fileObject.state = "puzzle2"
+        }
+        fs.writeFile('rooms/'+room_code+'.json', JSON.stringify(fileObject), (err) => {
+            // throws an error, you could also catch it here
+            if (err){ 
+                res.send({"failed": "player not added"});
+                throw err;
+            }
+            // success case, the file was save
+            res.send({"success": "puzzle1"});
+        });
+    }else{
+        res.send({"failed": "the room is full"})
+    }
+});
+app.get('/changestate', function (req = express.Request, res = express.Response) {
+    let room_code = req.query.room_code
+    let player_name = req.query.player_name
+    const filePath = 'rooms/'+room_code+'.json';
+    var fileObject = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    if(fileObject.players.length<=4){
+
+        fileObject.statearr.push(player_name)
+        if(fileObject.players.length == fileObject.statearr.length){
+            fileObject.state = "puzzle2"
+        }
+        fileObject.state = player_name
+        fs.writeFile('rooms/'+room_code+'.json', JSON.stringify(fileObject), (err) => {
+            // throws an error, you could also catch it here
+            if (err){ 
+                res.send({"failed": "player not added"});
+                throw err;
+            }
+            // success case, the file was save
+            res.send({"success": "puzzle1"});
+        });
+    }else{
+        res.send({"failed": "the room is full"})
+    }
+});
+app.get('/', function (req = express.Request, res = express.Response) {
+    res.send("E ae brasa hackers");
+})
